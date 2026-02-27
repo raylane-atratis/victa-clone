e7e3d175fa017de2bd6d03de0831045093a6a139
david-mendes-s <david.a.mendes.s@gmail.com>
2026-02-27 16:28:43 -0300

    Banner Seguro e Otimizado

--- wp-content/themes/atratis/template-parts/content-banner.php
+++ wp-content/themes/atratis/template-parts/content-banner.php
@@ -36,13 +36,14 @@ $mobileQuery = new WP_Query($mobileArgs);
 		<div class="swiper swiper-banner-principal">
 			<div class="swiper-wrapper">
 				<?php while ($desktopQuery->have_posts()) : $desktopQuery->the_post();
-					$img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+					// Imagem otimizada (fallback para full se tamanho registrado não existir)
+					$img_url = get_the_post_thumbnail_url(get_the_ID(), 'banner-desktop') ?: get_the_post_thumbnail_url(get_the_ID(), 'full');
 					$tamanho_imagem = 'cover'; // ou contain, auto. Poderia ser um campo ACF.
 
-					// Campos ACF
-					$titulo = get_field('banner_titulo_html');
-					$subtitulo = get_field('subtitulo_html');
-					$conteudo = get_field('banner_descricao');
+					// Campos ACF (Sanitizados com wp_kses_post para segurança)
+					$titulo = wp_kses_post(get_field('banner_titulo_html'));
+					$subtitulo = wp_kses_post(get_field('subtitulo_html'));
+					$conteudo = wp_kses_post(get_field('banner_descricao'));
 					$btnNome = get_field('nome_btn');
 					$btnLink = get_field('link_btn');
 					$novaAba = get_field('nova_aba');
@@ -89,10 +90,10 @@ $mobileQuery = new WP_Query($mobileArgs);
 										<?php endif; ?>
 
 										<?php if ($btnNome && $btnLink) : ?>
-											<a href="<?php echo esc_url($btnLink); ?>" class="btn" target="<?php echo $target; ?>" title="simular agora">
+											<a href="<?php echo esc_url($btnLink); ?>" class="btn" target="<?php echo esc_attr($target); ?>" title="<?php echo esc_attr($btnNome); ?>">
 												<?php echo esc_html($btnNome); ?>
-												<div class="svg-icon">
-													<svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+												<div class="svg-icon" aria-hidden="true">
+													<svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none" aria-hidden="true" focusable="false">
 														<path d="M9.85292 0H4.11754V1.37649H8.65033L0 10.0268L0.973179 11L9.62351 2.34967V6.88246H11V1.14708C11 0.514808 10.4857 0 9.85292 0Z" fill="white"/>
 													</svg>	
 												</div>
@@ -118,18 +119,25 @@ $mobileQuery = new WP_Query($mobileArgs);
 		<div class="swiper swiper-banner-mobile">
 			<div class="swiper-wrapper">
 				<?php while ($mobileQuery->have_posts()) : $mobileQuery->the_post();
-					$img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+					// Imagem otimizada (fallback para full)
+					$img_url = get_the_post_thumbnail_url(get_the_ID(), 'banner-mobile') ?: get_the_post_thumbnail_url(get_the_ID(), 'full');
+
+					// LCP condicional: primeiro slide = eager, demais = lazy
+					$is_first = ($mobileQuery->current_post === 0);
+					$loading_attr = $is_first ? 'eager' : 'lazy';
+					$priority_attr = $is_first ? 'fetchpriority="high"' : '';
+
 					$btnLink = get_field('link_btn');
 					$novaAba = get_field('nova_aba');
 					$target = $novaAba ? '_blank' : '_self';
 				?>
 					<div class="swiper-slide">
 						<?php if ($btnLink) : ?>
-							<a href="<?php echo esc_url($btnLink); ?>" target="<?php echo $target; ?>" class="d-block w-100 h-100">
-								<img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title_attribute(); ?>" class="w-100 h-auto" loading="eager" fetchpriority="high">
+							<a href="<?php echo esc_url($btnLink); ?>" target="<?php echo esc_attr($target); ?>" class="d-block w-100 h-100">
+								<img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title_attribute(); ?>" class="w-100 h-auto" loading="<?php echo $loading_attr; ?>" <?php echo $priority_attr; ?>>
 							</a>
 						<?php else : ?>
-							<img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title_attribute(); ?>" class="w-100 h-auto" loading="eager" fetchpriority="high">
+							<img src="<?php echo esc_url($img_url); ?>" alt="<?php the_title_attribute(); ?>" class="w-100 h-auto" loading="<?php echo $loading_attr; ?>" <?php echo $priority_attr; ?>>
 						<?php endif; ?>
 					</div>
 				<?php endwhile; wp_reset_postdata(); ?>
