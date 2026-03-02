4f4a70bd2e8d184950af9bf90f5c216caaabfe5c
david-mendes-s <david.a.mendes.s@gmail.com>
2026-03-02 10:38:25 -0300

    Inicio Footer

--- wp-content/themes/atratis/footer.php
+++ wp-content/themes/atratis/footer.php
@@ -1,12 +1,8 @@
 <?php
-$logo_principal = get_field('logo_image', 'option');
 $logo_rodape = get_field('logo_rodape', 'option');
 $lista_contatos = get_field('lista_contatos', 'option');
 $email = get_field('email', 'option');
-$horario_de_atendimento = get_field('horario_de_atendimento', 'option');
 $enderecos = get_field('enderecos', 'option');
-$cnpj = get_field('cnpj', 'option');
-$razao_social = get_field('razao_social', 'option');
 
 
 $lista_de_redes = get_field('lista_de_redes', 'option');
@@ -15,7 +11,57 @@ $lista_de_redes = get_field('lista_de_redes', 'option');
 </main><!-- #main-content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				
+			</div>
+			<div class="col-lg-3">
+				
+			</div>
+			<div class="col-lg-3">
+				
+			</div>
+			<div class="col-lg-3">
+				
+			</div>
+		</div>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="footer-bottom">
+          <div class="footer-bottom-left">
+            <p>
+              Marcelo Mota Advocacia
+            </p>
+          </div>
+          <div class="footer-bottom-center">
+            <div class="icon-logo">
+              <svg width="37" height="32" viewBox="0 0 37 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_8054_86)">
+                <path d="M11.3781 0L8.76637 7.8446L6.03419 0H3.17175V1.0591L7.47762 11.7805H10.0543L14.3602 1.0591V0H11.3781Z" fill="#157FFF"/>
+                <path d="M0.309326 0V16.0623H3.17176V7.11487V0H0.309326Z" fill="#157FFF"/>
+                <path d="M14.3602 0V16.0623H17.2235V7.11487V0H14.3602Z" fill="#157FFF"/>
+                <path d="M37 6.58008H14.3602V9.48249H23.0656H37V6.58008Z" fill="#157FFF"/>
+                <path d="M25.9305 15.9377L28.543 23.7823L31.2743 15.9377H34.1368V16.9968L29.8309 27.7183H27.2542L22.9484 16.9968V15.9377H25.9305Z" fill="#157FFF"/>
+                <path d="M37 15.9377V32.0001H34.1367V23.0526V15.9377H37Z" fill="#157FFF"/>
+                <path d="M22.9484 15.9377V32.0001H20.0859V23.0526V15.9377H22.9484Z" fill="#157FFF"/>
+                <path d="M0 22.5176H22.9483V25.42H14.1241H0V22.5176Z" fill="#157FFF"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_8054_86">
+                <rect width="37" height="32" fill="white"/>
+                </clipPath>
+                </defs>
+              </svg>
+            </div>
+          </div>
+          <div class="footer-bottom-right">
+            <p>Desenvolvido por <a href="https://atratis.com.br">Atratis</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+	</div>
 </footer>
 
 
